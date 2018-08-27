5672b70c5e1257e499ee1906259bab628b0e6d37
LuisTf <luistf.book@gmail.com>
2018-08-26 22:05:03 -0300

    create map3D and Authorization system

--- QueroConhecer/mapViewController.swift
+++ QueroConhecer/mapViewController.swift
@@ -11,6 +11,11 @@ import MapKit
 
 class mapViewController: UIViewController {
 
+    enum MapMessageType {
+        case routeError
+        case authorizationWarning
+    }
+    
     //MARK: Outltes
     @IBOutlet weak var seacrBar: UISearchBar!
     @IBOutlet weak var mapView: MKMapView!
@@ -22,6 +27,9 @@ class mapViewController: UIViewController {
     //MARK: Properties
     var places: [Place]!
     var poi: [MKAnnotation] = []
+    lazy var locationManager = CLLocationManager()
+    var btUserLocation: MKUserTrackingButton!
+    var selectedAnnotaion: PlaceAnnotaion?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,6 +37,7 @@ class mapViewController: UIViewController {
         viInfo.isHidden = true
         mapView.mapType = .mutedStandard
         mapView.delegate = self
+        locationManager.delegate = self
         
         if places.count == 1 {
             title = places[0].name
@@ -40,10 +49,23 @@ class mapViewController: UIViewController {
             addToMap(place)
         }
         
+        configureLocationButton()
+        
         showPlaces()
+        requestUserLocationAuthorization()
     }
     
     //MARK: Methods
+    func configureLocationButton() {
+        btUserLocation = MKUserTrackingButton(mapView: mapView)
+        btUserLocation.backgroundColor = .white
+        btUserLocation.frame.origin.x = 10
+        btUserLocation.frame.origin.y = 10
+        btUserLocation.layer.cornerRadius = 5
+        btUserLocation.layer.borderWidth = 1
+        btUserLocation.layer.borderColor = UIColor(named: "main")?.cgColor
+    }
+    
     func addToMap(_ place: Place) {
         let annotation = PlaceAnnotaion(coordinate: place.coordinate, type: .place)
         annotation.coordinate = place.coordinate
@@ -54,6 +76,56 @@ class mapViewController: UIViewController {
     
     func showPlaces() {
         mapView.showAnnotations(mapView.annotations, animated: true)
+    }
+    
+    func requestUserLocationAuthorization() {
+        if CLLocationManager.locationServicesEnabled() {
+            switch CLLocationManager.authorizationStatus() {
+                case .authorizedAlways, .authorizedWhenInUse:
+                    mapView.addSubview(btUserLocation)
+                case .denied:
+                    showMessage(type: .authorizationWarning)
+                case .notDetermined:
+                    locationManager.requestWhenInUseAuthorization()
+                case .restricted:
+                    break
+            }
+        } else {
+            
+        }
+    }
+    
+    func showMessage(type: MapMessageType) {
+//        let title: String
+//        let message: String
+//        var hasConfirmation: Bool = false
+//        switch type {
+//        case .confirmation(let name):
+//            title = "Local encontrado"
+//            message = "Deseja adicionar \(name)?"
+//            hasConfirmation = true
+//        case .error(let errorMessage):
+//            title = "Error"
+//            message = errorMessage
+//        }
+//
+//        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+//        let cancelAction = UIAlertAction(title: "Cancelar", style: .cancel, handler: nil)
+//        alert.addAction(cancelAction)
+//        if hasConfirmation {
+//            let confirmAction = UIAlertAction(title: "Ok", style: .default) { (action) in
+//                self.delegate?.addPlace(self.place)
+//                self.dismiss(animated: true, completion: nil)
+//            }
+//            alert.addAction(confirmAction)
+//        }
+//        present(alert, animated: true, completion: nil)
+    }
+    
+    func showInfo() {
+        lbName.text = selectedAnnotaion!.title
+        lbAddress.text = selectedAnnotaion!.address
+        viInfo.isHidden = false
     }
 
 
@@ -89,6 +161,18 @@ extension mapViewController: MKMapViewDelegate {
         return annotaionView
     }
     
+    func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+        
+        let camera = MKMapCamera()
+        camera.centerCoordinate = view.annotation!.coordinate
+        camera.pitch = 80
+        camera.altitude = 100
+        mapView.setCamera(camera, animated: true)
+        
+        selectedAnnotaion = (view.annotation as! PlaceAnnotaion)
+        showInfo()
+    }
+    
 }
 
 extension mapViewController: UISearchBarDelegate {
@@ -121,6 +205,23 @@ extension mapViewController: UISearchBarDelegate {
             }
             self.loading.stopAnimating()
         }
+    }
+}
+
+extension mapViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        switch status {
+            case .authorizedAlways, .authorizedWhenInUse:
+                mapView.showsUserLocation = true
+                mapView.addSubview(btUserLocation)
+                locationManager.startUpdatingLocation()
+            default:
+                break
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        print(locations.last!)
     }
 }
 
